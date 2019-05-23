209bbd40537b216ed9c0f47ddb174d7096a771c9
mike011 <mike_ch_1@hotmail.com>
2019-05-23 06:46:41 -0400

    got Layer Animations started

---
+++ AnimationPreview/AnimationPreview/Layer Animations/LayerAnimationsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LayerAnimationsViewController.swift
+//  AnimationPreview
+//
+//  Created by Michael Charland on 2019-05-23.
+//  Copyright © 2019 charland. All rights reserved.
+//
+
+import UIKit
+
+class LayerAnimationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
+
+extension LayerAnimationsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+
+}
